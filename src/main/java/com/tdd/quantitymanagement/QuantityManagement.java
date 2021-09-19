f104028ec3f18181150efb30c8805981b5a0f1c9
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-19 17:42:30 +0530

    Added: Project files

---
+++ src/main/java/com/tdd/quantitymanagement/QuantityManagement.java
@@ -0,0 +1,13 @@
+package com.tdd.quantitymanagement;
+
+/**
+ * Quantity Management
+ *
+ */
+public class QuantityManagement
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Quantity Management Program" );
+    }
+}
---
+++ src/test/java/com/tdd/quantitymanagement/QuantityManagementTest.java
@@ -0,0 +1,17 @@
+package com.tdd.quantitymanagement;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple QuantityManagement.
+ */
+public class QuantityManagementTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
